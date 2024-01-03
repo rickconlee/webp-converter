a819731509935772f2876fbc1a323bd3013e95b6
Rick Conlee <rick.conlee@gmail.com>
2024-01-02 20:03:48 -0800

    Adding Tiff Handling

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module webp-converter
 
 go 1.21.5
 
-require github.com/chai2010/webp v1.1.1 // indirect
+require (
+	github.com/chai2010/webp v1.1.1 // indirect
+	golang.org/x/image v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,75 +1,76 @@
 package main
 
 import (
-    "fmt"
-    "image"
-    // Import with underscore to register formats without direct usage
-    _ "image/jpeg"
-    _ "image/png"
-    "os"
-    "path/filepath"
-    "strings"
+	"fmt"
+	"image"
+	_ "image/gif"  // Register GIF format
+	_ "image/jpeg" // Register JPEG format
+	_ "image/png"  // Register PNG format
+	"os"
+	"path/filepath"
+	"strings"
 
-    _ "image/gif" // Register GIF format
-    "github.com/chai2010/webp"
+	_ "golang.org/x/image/tiff" // Register TIFF format
+
+	"github.com/chai2010/webp"
 )
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("Usage: go run main.go <directory_path>")
-        os.Exit(1)
-    }
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: $ webpconvert <directory_path>")
+		os.Exit(1)
+	}
 
-    directoryPath := os.Args[1]
-    err := convertDirectoryToWebP(directoryPath)
-    if err != nil {
-        fmt.Printf("Error converting images: %v\n", err)
-        os.Exit(1)
-    }
+	directoryPath := os.Args[1]
+	err := convertDirectoryToWebP(directoryPath)
+	if err != nil {
+		fmt.Printf("Error converting images: %v\n", err)
+		os.Exit(1)
+	}
 
-    fmt.Println("All conversions successful!")
+	fmt.Println("All conversions successful!")
 }
 
 func convertDirectoryToWebP(dirPath string) error {
-    return filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return err
-        }
-        if !info.IsDir() && isSupportedFormat(path) {
-            err := convertToWebP(path)
-            if err != nil {
-                fmt.Printf("Failed to convert %s: %v\n", path, err)
-            } else {
-                fmt.Printf("Converted %s successfully\n", path)
-            }
-        }
-        return nil
-    })
+	return filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && isSupportedFormat(path) {
+			err := convertToWebP(path)
+			if err != nil {
+				fmt.Printf("Failed to convert %s: %v\n", path, err)
+			} else {
+				fmt.Printf("Converted %s successfully\n", path)
+			}
+		}
+		return nil
+	})
 }
 
 func isSupportedFormat(filePath string) bool {
-    ext := strings.ToLower(filepath.Ext(filePath))
-    return ext == ".jpg" || ext == ".jpeg" || ext == ".png" || ext == ".gif"
+	ext := strings.ToLower(filepath.Ext(filePath))
+	return ext == ".jpg" || ext == ".jpeg" || ext == ".png" || ext == ".gif" || ext == ".tiff"
 }
 
 func convertToWebP(inputPath string) error {
-    file, err := os.Open(inputPath)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
+	file, err := os.Open(inputPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-    img, _, err := image.Decode(file)
-    if err != nil {
-        return err
-    }
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return err
+	}
 
-    outputFileName := strings.TrimSuffix(inputPath, filepath.Ext(inputPath)) + ".webp"
-    outputFile, err := os.Create(outputFileName)
-    if err != nil {
-        return err
-    }
-    defer outputFile.Close()
+	outputFileName := strings.TrimSuffix(inputPath, filepath.Ext(inputPath)) + ".webp"
+	outputFile, err := os.Create(outputFileName)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
 
-    return webp.Encode(outputFile, img, &webp.Options{Lossless: true})
+	return webp.Encode(outputFile, img, &webp.Options{Lossless: true})
 }
